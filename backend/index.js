8628a673f7b1be7586973a41651427ca99b8de35
hem dhoni <hemdhoni@hems-MacBook-Air.local>
2025-01-03 09:58:02 +0530

    authentication done

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,33 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const express = require("express")
+const promClient = require("prom-client") //metrics collections
 const app  = express()
+const cookieParser = require("cookie-parser")
 const dbconnect = require("./db/db")
 const userRoutes = require("./routes/user.routes")
 
 
 const cors = require("cors")
 
+const collectDefaultMetrics = promClient.collectDefaultMetrics
+collectDefaultMetrics({ register: promClient.register })
+
 dbconnect() //db connection
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 
 app.get("/" , (req,res) => {
     res.send("Hello World !")
+})
+
+app.get("/metrics" ,async  (req,res) => {
+    res.setHeader("Content-Type" , promClient.register.contentType)
+    const metrics = await promClient.register.metrics()
+    res.send(metrics)
 })
 
 app.use("/users" , userRoutes)
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,13 +2,23 @@ const express = require("express")
 const router  = express.Router();
 const { validationResult, body } = require("express-validator")
 const userController = require("../controllers/user.controller")
-
+const authMiddleware = require("../middlewares/auth.middleware")   
 
 router.post("/register" , [
     body("email").isEmail().withMessage("Invalid Email"),
     body("fullname.firstname").isLength({min:3}).withMessage("FirstName is required."),
     body("password").isLength({min:6}).withMessage("Password must be 6 character.")
 ] ,userController.registerUser)
+
+
+router.post("/login" , [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password").isLength({min:6}).withMessage("Password must be 6 character.")
+] ,userController.loginUser)
+
+router.get("/profile" , authMiddleware.authUser, userController.getUserProfile)
+router.get("/logout" , authMiddleware.authUser, userController.logoutUser)
+
 
 
 
